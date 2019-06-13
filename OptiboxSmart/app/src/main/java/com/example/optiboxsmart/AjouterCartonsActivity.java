fd363fc8c9e65c59cfe9e3d2862763fbc04edd7a
LoulouW8andC <louis.hocquet@sfr.fr>
2019-06-13 10:34:38 +0200

    Gestion du click sur AjoutCartonsActivity

--- OptiboxSmart/app/src/main/java/com/example/optiboxsmart/AjouterCartonsActivity.java
+++ OptiboxSmart/app/src/main/java/com/example/optiboxsmart/AjouterCartonsActivity.java
@@ -3,13 +3,19 @@ package com.example.optiboxsmart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class AjouterCartonsActivity extends AppCompatActivity {
+public class AjouterCartonsActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Map<Integer, Integer> mapCartons;
     private TextView tvNbType1;
@@ -30,12 +36,24 @@ public class AjouterCartonsActivity extends AppCompatActivity {
     private Button btnRetraitType4;
     private Button btnRetraitType5;
     private Button btnRetraitType6;
+    private List<TextView> listNbCartons;
+    private List<Button> listBtnAjout;
+    private List<Button> listBtnRetrait;
+
+    private static String CAT = "PMR";
+    // Fonction alerter()
+    private void alerter(String s){
+        Toast toastAlert = Toast.makeText(this, s, Toast.LENGTH_SHORT);
+        toastAlert.show();
+        Log.i(CAT, s);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter_cartons);
         mapCartons = new HashMap<>();
+
     }
 
     @Override
@@ -53,12 +71,29 @@ public class AjouterCartonsActivity extends AppCompatActivity {
         btnRetraitType4 = findViewById(R.id.btnRetraitType4);
         btnRetraitType5 = findViewById(R.id.btnRetraitType5);
         btnRetraitType6 = findViewById(R.id.btnRetraitType6);
-        tvNbType1 = findViewById(R.id.tvNbType1);
-        tvNbType2 = findViewById(R.id.tvNbType2);
+        tvNbType1 = findViewById(R.id.tvType1);
+        tvNbType2 = findViewById(R.id.tvType2);
         tvNbType3 = findViewById(R.id.tvType3);
         tvNbType4 = findViewById(R.id.tvType4);
         tvNbType5 = findViewById(R.id.tvType5);
         tvNbType6 = findViewById(R.id.tvType6);
+
+        for (int i = 0; i < 6; i++) {
+            mapCartons.put(i+1, 0);
+        }
+
+        listNbCartons = new ArrayList<>(Arrays.asList(tvNbType1, tvNbType2, tvNbType3,
+                tvNbType4, tvNbType5, tvNbType6));
+        listBtnAjout =  new ArrayList<>(Arrays.asList(btnAjoutType1, btnAjoutType2, btnAjoutType3,
+                btnAjoutType4, btnAjoutType5, btnAjoutType6));
+        listBtnRetrait = new ArrayList<>(Arrays.asList(btnRetraitType1, btnRetraitType2, btnRetraitType3,
+                btnRetraitType4, btnRetraitType5, btnRetraitType6));
+
+        // ########### Mise en place d'un gestionnaire de clics ############
+        for (int i = 0; i < 6; i++) {
+            listBtnRetrait.get(i).setOnClickListener(this);
+            listBtnAjout.get(i).setOnClickListener(this);
+        }
     }
 
     // ########### Fonctions gestion des cartons #########
@@ -73,8 +108,11 @@ public class AjouterCartonsActivity extends AppCompatActivity {
         if (typeCarton > 6 || typeCarton < 1) return false;
         else{
             int nbCartons = mapCartons.get(typeCarton);
-            mapCartons.put(typeCarton, nbCartons++);
-            actualisationNbCartons();
+            nbCartons++;
+
+            mapCartons.put(typeCarton, nbCartons);
+//            alerter(String.valueOf(mapCartons.get(typeCarton)));
+            listNbCartons.get(typeCarton-1).setText(String.valueOf(nbCartons));
             return true;
         }
     }
@@ -90,21 +128,68 @@ public class AjouterCartonsActivity extends AppCompatActivity {
         else{
             int nbCartons = mapCartons.get(typeCarton);
             nbCartons--;
-            mapCartons.put(typeCarton, Math.max(nbCartons, 0));
-            actualisationNbCartons();
+            nbCartons = Math.max(nbCartons, 0);
+            mapCartons.put(typeCarton, nbCartons);
+            listNbCartons.get(typeCarton-1).setText(String.valueOf(nbCartons));
             return true;
         }
     }
 
     /**
      *
-     * @return true 
+     * @return true
      */
     public boolean actualisationNbCartons(){
-        for (int i = 1; i < 6; i++) {
+        for (int i = 1; i < 7; i++) {
             int nbCartons = mapCartons.get(i);
-                tvNbType1.setText(nbCartons);
+            alerter(String.valueOf(nbCartons));
+                tvNbType1.setText(String.valueOf(nbCartons));
         }
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.btnAjoutType1:
+                ajoutCarton(1);
+                break;
+            case R.id.btnRetraitType1:
+                retraitCarton(1);
+                break;
+
+            case R.id.btnAjoutType2:
+                ajoutCarton(2);
+                break;
+            case R.id.btnRetraitType2:
+                retraitCarton(2);
+                break;
+            case R.id.btnAjoutType3:
+                ajoutCarton(3);
+                break;
+            case R.id.btnRetraitType3:
+                retraitCarton(3);
+                break;
+            case R.id.btnAjoutType4:
+                ajoutCarton(4);
+                break;
+            case R.id.btnRetraitType4:
+                retraitCarton(4);
+                break;
+            case R.id.btnAjoutType5:
+                ajoutCarton(5);
+                break;
+            case R.id.btnRetraitType5:
+                retraitCarton(5);
+                break;
+            case R.id.btnAjoutType6:
+                ajoutCarton(6);
+                break;
+            case R.id.btnRetraitType6:
+                retraitCarton(6);
+                break;
+        }
     }
 }
--- OptiboxSmart/app/src/main/java/com/example/optiboxsmart/MainActivity.java
+++ OptiboxSmart/app/src/main/java/com/example/optiboxsmart/MainActivity.java
@@ -2,15 +2,17 @@ package com.example.optiboxsmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +21,19 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnStart:
+                Intent toAjouterCartonsActivity = new Intent(this, AjouterCartonsActivity.class);
+                startActivity(toAjouterCartonsActivity);
+        }
+    }
 }
