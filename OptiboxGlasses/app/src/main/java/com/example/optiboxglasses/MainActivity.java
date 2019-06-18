3f53ab2c57e704a0c9815f3f82bbc5730ddf54cd
LoulouW8andC <louis.hocquet@sfr.fr>
2019-06-18 14:37:29 +0200

    Préparation des fonctions de scan des devices Bluetooth (OptiboxSmart) et de détectabilité (OptiboxGlasses)

--- OptiboxGlasses/app/src/main/java/com/example/optiboxglasses/MainActivity.java
+++ OptiboxGlasses/app/src/main/java/com/example/optiboxglasses/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.optiboxglasses;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnStart:
+                Intent toResceptionBluetooth = new Intent(this, ReceptionBluetooth.class);
+                startActivity(toResceptionBluetooth);
+        }
     }
 }
