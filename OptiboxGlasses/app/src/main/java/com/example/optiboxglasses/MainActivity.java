9d853059f373156a3d47e5810812e6f5ef3ea266
Samuel Chaput <samuelchaput87@gmail.com>
2019-06-20 10:50:29 +0200

    Speech recognizer added

--- OptiboxGlasses/app/src/main/java/com/example/optiboxglasses/MainActivity.java
+++ OptiboxGlasses/app/src/main/java/com/example/optiboxglasses/MainActivity.java
@@ -2,24 +2,21 @@ package com.example.optiboxglasses;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
 import java.util.ArrayList;
-import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private static final int REQUEST_CODE = 10;
     private TextView textOutput;
-    private Button btnStart;
+
+    private SpeechRecognizer speechRecognizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,44 +24,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         textOutput= findViewById(R.id.textOutput);
-        btnStart = findViewById(R.id.startDictation);
 
-        btnStart.setOnClickListener(this);
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        speechRecognizer.setRecognitionListener(new SpeechListener());
+        startRecognition();
     }
 
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.startDictation :
-                // intent to google's speech recognition (works offline too)
-                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+    public void startRecognition() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,"voice.recognition.test");
 
-                try {
-                    //Start the Activity and wait for the response
-                    startActivityForResult(intent, REQUEST_CODE);
-                    Log.d(TAG, "onClick: success starting !");
-                } catch (ActivityNotFoundException a) {
-                    Log.d(TAG, "onClick: error : catch : "+a.toString());
-                }
-                break;
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,5);
+        speechRecognizer.startListening(intent);
+    }
+
+    class SpeechListener implements RecognitionListener {
+        public void onReadyForSpeech(Bundle params)
+        {
+            Log.d(TAG, "onReadyForSpeech");
         }
-    }
-
-    //Handle the results//
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case REQUEST_CODE: {
-                if (resultCode == RESULT_OK && data != null) {
-                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    textOutput.setText(result.get(0));
-                }
-                break;
+        public void onBeginningOfSpeech()
+        {
+            Log.d(TAG, "onBeginningOfSpeech");
+        }
+        public void onRmsChanged(float rmsdB)
+        {
+//            Log.d(TAG, "onRmsChanged");
+        }
+        public void onBufferReceived(byte[] buffer)
+        {
+            Log.d(TAG, "onBufferReceived");
+        }
+        public void onEndOfSpeech()
+        {
+            Log.d(TAG, "onEndofSpeech");
+        }
+        public void onError(int error)
+        {
+            Log.d(TAG,  "error " +  error);
+            textOutput.setText("error " + error);
+            startRecognition();
+        }
+        public void onResults(Bundle results)
+        {
+            String str = new String();
+            Log.d(TAG, "onResults " + results);
+            ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for (int i = 0; i < data.size(); i++)
+            {
+                Log.d(TAG, "result " + data.get(i));
+                str += data.get(i)+" ";
             }
+            textOutput.setText("results: "+str);
+            if (!str.contains("next"))
+                startRecognition();
+        }
+        public void onPartialResults(Bundle partialResults)
+        {
+            Log.d(TAG, "onPartialResults");
+        }
+        public void onEvent(int eventType, Bundle params)
+        {
+            Log.d(TAG, "onEvent " + eventType);
         }
     }
+
+
 }
